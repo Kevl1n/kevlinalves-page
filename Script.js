44adced729e11a1427ff7ff42276c253c35c1c07
Kevl1n <alves.kevlin@gmail.com>
2025-05-19 18:55:42 -0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,5 @@
+document.getElementById('botao').addEventListener('click', function() {
+        const mensagem = document.getElementById('mensagem');
+        mensagem.textContent = "O sentido da vida é ser feliz, lembre-se de não levar as coisas tão a sério sempre.";
+        mensagem.style.display = "block";
+    });
